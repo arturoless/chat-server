bdc81f4e144c094e020d79a7656a236b550d40ce
arturoless <43190646+arturoless@users.noreply.github.com>
2019-03-31 22:31:35 -0600

    a

--- app.js
+++ app.js
@@ -1,29 +1,83 @@
-var express = require("express");
-var app = new express();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
-const os = require('os')
-var interfaces = os.networkInterfaces()
-var wifi = interfaces['eth0']
-if(wifi==null){
-    wifi = interfaces['Wi-Fi']
-    var ipv4 = wifi[1]
-}
-else{
-    var ipv4 = wifi[0]
-}
-var PORT = process.env.PORT || 3000;
-var HOST = ipv4['address']
-io.on('connection',function(socket){
-    socket.on('chat1',function(mensaje){
-        socket.broadcast.emit('chat2',mensaje);  
-    });
-    socket.on('chat2',function(mensaje){
-        socket.broadcast.emit('chat1',mensaje);  
-    });
- 
+"use strict";
+const express = require('express');
+const app = express();
+const http = require('http');
+const httpServer = http.Server(app);
+const io = require('socket.io')(httpServer);
+const SocketIOFile = require('socket.io-file');
+const cors = require('cors');
+// app.get('/', (req, res, next) => {
+// 	return res.sendFile(__dirname + '/client/index.html');
+// });
+
+// app.get('/app.js', (req, res, next) => {
+// 	return res.sendFile(__dirname + '/client/app.js');
+// });
+
+// app.get('/socket.io.js', (req, res, next) => {
+// 	return res.sendFile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
+// });
+
+// app.get('/socket.io-file-client.js', (req, res, next) => {
+// 	return res.sendFile(__dirname + '/node_modules/socket.io-file-client/socket.io-file-client.js');
+// });
+
+app.use(cors());
+
+io.origins('*:*')
+
+app.use('/data', express.static('data'));
+
+io.on('connection', (socket) => {
+	console.log('Socket connected.');
+	socket.on('maria', (datos)=>{
+        socket.broadcast.emit('maria', datos);
+    })
+    socket.on('juan',function (datos) {
+        console.log(datos)
+        socket.broadcast.emit('juan', datos);
+    })
+    socket.on('callbackmaria',(callback)=>{
+        socket.broadcast.emit('callbackmaria', callback);
+    })
+    socket.on('callbackjuan',(callback)=>{
+        socket.broadcast.emit('callbackjuan', callback);
+    })
+	var uploader = new SocketIOFile(socket, {
+		uploadDir: 'data',							// simple directory
+		// accepts: ['audio/mpeg', 'audio/mp3'],		// chrome and some of browsers checking mp3 as 'audio/mp3', not 'audio/mpeg'
+		// maxFileSize: 4194304, 						// 4 MB. default is undefined(no limit)
+		chunkSize: 10240,							// default is 10240(1KB)
+		transmissionDelay: 0,						// delay of each transmission, higher value saves more cpu resources, lower upload speed. default is 0(no delay)
+		overwrite: false, 							// overwrite file if exists, default is true.
+		// rename: function(filename) {
+		// 	var split = filename.split('.');	// split filename by .(extension)
+		// 	var fname = split[0];	// filename without extension
+		// 	var ext = split[1];
+
+		// 	return `${fname}_${count++}.${ext}`;
+		// }
+		// rename: 'MyMusic.mp3'
+	});
+	uploader.on('start', (fileInfo) => {
+		console.log('Start uploading');
+		console.log(fileInfo);
+	});
+	uploader.on('stream', (fileInfo) => {
+		console.log(`${fileInfo.wrote} / ${fileInfo.size} byte(s)`);
+	});
+	uploader.on('complete', (fileInfo) => {
+		console.log('Upload Complete.');
+		console.log(fileInfo);
+	});
+	uploader.on('error', (err) => {
+		console.log('Error!', err);
+	});
+	uploader.on('abort', (fileInfo) => {
+		console.log('Aborted: ', fileInfo);
+	});
 });
- 
-http.listen(PORT,function(){
-console.log(HOST+':'+PORT)
+
+httpServer.listen(3030, () => {
+	console.log('Server listening on port 3030');
 });
